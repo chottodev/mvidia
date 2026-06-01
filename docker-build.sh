8362288d805ad321377f9f5296b2c8a267bb44c7
sergey <110@mobilon.ru>
2026-06-02 01:31:40 +0700

    fix

--- docker-build.sh
+++ docker-build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Один образ antirek/mvidia:TAG — в compose два сервиса с разным command.
 
-TAG="${TAG:-0.0.10}"
+TAG="${TAG:-0.0.11}"
 IMAGE="${IMAGE:-antirek/mvidia}"
 
 docker build -t "${IMAGE}:${TAG}" .
